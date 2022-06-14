b3f90b826bd3636987d35363f86ca5e8dacfa18b
Isis Zapata <92564676+isinicolle@users.noreply.github.com>
2022-06-14 14:33:57 -0600

    Create ejercicios.cpp

---
+++ C++/ejercicios.cpp
@@ -0,0 +1,116 @@
+//ejercicio de suma de dos numeros
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, suma;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    suma = num1 + num2;
+    cout << "La suma de los numeros es: " << suma;
+    return 0;
+}
+//ejercicio de resta de dos numeros
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, resta;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    resta = num1 - num2;
+    cout << "La resta de los numeros es: " << resta;
+    return 0;
+}
+//ejercicio de multiplicacion de dos numeros
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, multiplicacion;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    multiplicacion = num1 * num2;
+    cout << "La multiplicacion de los numeros es: " << multiplicacion;
+    return 0;
+}
+//ejercicio de division de dos numeros
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, division;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    division = num1 / num2;
+    cout << "La division de los numeros es: " << division;
+    return 0;
+}
+
+//pilas y colas
+//ejercicio de pilas
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    cout << "Ingrese el tercer numero: ";
+    cin >> num3;
+    cout << "Ingrese el cuarto numero: ";
+    cin >> num4;
+    cout << "Ingrese el quinto numero: ";
+    cin >> num5;
+    cout << "Ingrese el sexto numero: ";
+    cin >> num6;
+    cout << "Ingrese el septimo numero: ";
+    cin >> num7;
+    cout << "Ingrese el octavo numero: ";
+    cin >> num8;
+    cout << "Ingrese el noveno numero: ";
+    cin >> num9;
+    cout << "Ingrese el decimo numero: ";
+    cin >> num10;
+    cout << "Los numeros ingresados son: " << num1 << " " << num2 << " " << num3 << " " << num4 << " " << num5 << " " << num6 << " " << num7 << " " << num8 << " " << num9 << " " << num10;
+    return 0;
+}
+//ejercicio de colas
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    cout << "Ingrese el tercer numero: ";
+    cin >> num3;
+    cout << "Ingrese el cuarto numero: ";
+    cin >> num4;
+    cout << "Ingrese el quinto numero: ";
+    cin >> num5;
+    cout << "Ingrese el sexto numero: ";
+    cin >> num6;
+    cout << "Ingrese el septimo numero: ";
+    cin >> num7;
+    cout << "Ingrese el octavo numero: ";
+    cin >> num8;
+    cout << "Ingrese el noveno numero: ";
+    cin >> num9;
+    cout << "Ingrese el decimo numero: ";
+    cin >> num10;
+    cout << "Los numeros ingresados son: " << num1 << " " << num2 << " " << num3 << " " << num4 << " " << num5 << " " << num6 << " " << num7 << " " << num8 << " " << num9 << " " << num10;
+    return 0;
+}
